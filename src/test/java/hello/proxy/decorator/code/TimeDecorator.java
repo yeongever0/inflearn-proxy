8d1e502cb5012ec31a3299ebe2b5492bfb71da36
yeongever0 <yeongever0@gmail.com>
2024-03-18 17:03:36 +0900

    test: Decorator Pattern 적용2 - 체인 방식 적용

---
+++ src/test/java/hello/proxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,26 @@
+package hello.proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeDecorator implements Component{
+
+    private Component component;
+
+    public TimeDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String opration() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+
+        String result = component.opration();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeDecoder 종료 resultTime={}ms", resultTime);
+        return null;
+    }
+}

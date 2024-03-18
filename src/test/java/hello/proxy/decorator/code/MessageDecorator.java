118e604147d6bdf9f39d86bf882a5740a0532fd2
yeongever0 <yeongever0@gmail.com>
2024-03-18 16:57:52 +0900

    test: Decorator Pattern 적용 - `MessageDecorator` 적용

---
+++ src/test/java/hello/proxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,22 @@
+package hello.proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component{
+
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String opration() {
+        log.info("MessageDecorator 실행");
+        String result = component.opration();
+        String decoResult = "*****" + result + "*****";
+        log.info("MessageDecorator 꾸미기 적용 전={}, 적용 후={}", result, decoResult);
+        return decoResult;
+    }
+}

fead84dcc69a4fbe36146939dc2b7b6343f6ecc7
yeongever0 <yeongever0@gmail.com>
2024-03-18 16:21:27 +0900

    test: Proxy Pattern 적용

--- src/test/java/hello/proxy/pureProxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureProxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureProxy;
 
+import hello.proxy.pureProxy.code.CacheProxy;
 import hello.proxy.pureProxy.code.ProxyPatternClient;
 import hello.proxy.pureProxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
